84c1bc863c444390ff7620e2439a7346aaa2d791
heewon-hub <gmldnjs2623@gmail.com>
2025-05-20 15:11:03 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 96 ms, Memory: 9328 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+
+  if (a > b) {
+    console.log('>');
+  } else if (a < b) {
+    console.log('<');
+  } else {
+    console.log('==');
+  }
