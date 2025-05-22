7ce5b7ac58d0f6eef7e8caf7645b3f214312f4be
heewon-hub <gmldnjs2623@gmail.com>
2025-05-22 12:35:18 +0900

    [Bronze V] Title: 개, Time: 140 ms, Memory: 9328 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/10172. 개/개.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().trim();
+
+console.log("|\\_/|"); 
+console.log("|q p|   /}");
+console.log('( 0 )"""\\');
+console.log('|"^"`    |');
+console.log("||_/=\\\\__|");
