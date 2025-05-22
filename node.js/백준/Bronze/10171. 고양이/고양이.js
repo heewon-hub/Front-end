b002ffefec213af2b477c654bc229eac296f2484
heewon-hub <gmldnjs2623@gmail.com>
2025-05-22 12:32:02 +0900

    [Bronze V] Title: 고양이, Time: 136 ms, Memory: 9320 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().trim();
+
+console.log("\\    /\\"); 
+console.log(" )  ( ')");
+console.log("(  /  )");
+console.log(" \\(__)|");
