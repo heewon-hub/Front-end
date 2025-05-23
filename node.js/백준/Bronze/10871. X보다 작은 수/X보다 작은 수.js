fdadd0f240ac47a98d3fa3b49590fd496e3a0b52
heewon-hub <gmldnjs2623@gmail.com>
2025-05-24 00:01:41 +0900

    [Bronze V] Title: X보다 작은 수, Time: 96 ms, Memory: 11060 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [N, X] = input[0].split(' ').map(Number);
+const A = input[1].split(' ').map(Number);
+
+const result = []
+
+for (let i = 0; i < N; i++) {
+  if (A[i] < X) {
+    result.push(A[i]);
+  }
+}
+
+console.log(result.join(' '));
