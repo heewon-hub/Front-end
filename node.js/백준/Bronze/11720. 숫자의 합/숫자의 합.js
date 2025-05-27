ce4d78bc05c48fd6ecdca256f091eb49f51d33b7
heewon-hub <gmldnjs2623@gmail.com>
2025-05-28 01:42:26 +0900

    [Bronze IV] Title: 숫자의 합, Time: 100 ms, Memory: 9584 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/11720. 숫자의 합/숫자의 합.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = input[0];
+const numbers = input[1];
+
+let sum = 0;
+for (let i = 0; i < N; i++) {
+    sum += Number(numbers[i]);
+}
+
+console.log(sum);
