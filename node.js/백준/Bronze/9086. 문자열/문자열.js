922b34e61a9fd7a1d04b507b2b4eb6d479fd1c66
heewon-hub <gmldnjs2623@gmail.com>
2025-06-11 14:32:27 +0900

    [Bronze V] Title: 문자열, Time: 88 ms, Memory: 9312 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/9086. 문자열/문자열.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const T = parseInt(input[0]);
+
+for (let i = 1; i <= T; i++) {
+    const str = input[i];
+    const firstChar = str[0];
+    const lastChar = str[str.length - 1];
+    console.log(firstChar + lastChar);
+}
