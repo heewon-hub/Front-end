b448e84ef0a25f3218f379a3ad6367a0f7f0c661
heewon-hub <gmldnjs2623@gmail.com>
2025-06-25 22:19:03 +0900

    [Bronze III] Title: 배수와 약수, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/5086. 배수와 약수/배수와 약수.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+for (let i = 0; i < input.length; i++) {
+  const [a, b] = input[i].split(" ").map(Number);
+
+  if (a === 0 && b === 0) break;
+
+  if (b % a === 0) {
+    console.log("factor");
+  } else if (a % b === 0) {
+    console.log("multiple");
+  } else {
+    console.log("neither");
+  }
+}
